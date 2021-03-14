a41015c52c33c66052a31742c7da34d60ad9d300
reinderien <gtoombs@lakeheadu.ca>
2021-03-14 13:32:50 -0400

    Partial iterative solution

--- satisfactory/demos.py
+++ satisfactory/demos.py
@@ -1,3 +1,5 @@
+from contextlib import contextmanager
+
 from .power import PowerSolver, ShardMode
 from .rates import setup_linprog, solve_linprog, get_clocks, get_rates
 from .recipe import load_recipes, graph_recipes
@@ -131,7 +133,7 @@ def big_tier_4():
     problem = setup_linprog(
         recipes,
         min_clocks={solved.recipe.name: round(solved.clock_total)
-                      for solved in approx.solved})
+                    for solved in approx.solved})
     solve_linprog(problem)
 
     with PowerSolver(recipes,
@@ -142,3 +144,71 @@ def big_tier_4():
         exact.solve()
         exact.print()
         exact.graph()
+
+
+def current():
+    print('Current gameplay based on fixed mine recipes')
+
+    def constraints(power):
+        return (
+            power.building_total <= 100,
+            power.power_total <= 300e6,
+            # power.shard_total <= 94,
+            power.buildings['Iron Ore from Miner Mk.2 on Pure node'] == 3,
+            power.buildings['Copper Ore from Miner Mk.2 on Pure node'] == 1,
+            power.buildings['Caterium Ore from Miner Mk.2 on Pure node'] == 1,
+        )
+
+    recipes = load_recipes(TIERS_TO_5)
+    @contextmanager
+    def solve(problem, **kwargs):
+        with PowerSolver(recipes,
+                         percentages=dict(get_clocks(problem)),
+                         rates=dict(get_rates(problem)),
+                         **kwargs) as power:
+            power.constraints(*constraints(power))
+            yield power
+            power.solve()
+
+    problem = setup_linprog(recipes,
+                            min_rates={
+                                'A.I. Limiter': 0.1,
+                                'Encased Industrial Beam': 0.1,
+                                'Heavy Modular Frame': 0.1,
+                                'Motor': 0.1,
+                                'Versatile Framework': 0.1,
+                            })
+    solve_linprog(problem)
+
+    for iteration in range(2):
+
+        with PowerSolver(recipes,
+                         percentages=dict(get_clocks(problem)),
+                         rates=dict(get_rates(problem)),
+                         # shard_mode=ShardMode.BINARY,
+                         scale_clock=True) as approx:
+
+            constraints =
+
+            approx.constraints(*constraints)
+            approx.maximize(approx.clock_totals['Versatile Framework'])
+            approx.solve()
+            approx.print()
+
+        problem = setup_linprog(
+            recipes,
+            min_clocks={solved.recipe.name: round(solved.clock_total)
+                        for solved in approx.solved})
+        solve_linprog(problem)
+
+        with PowerSolver(recipes,
+                         percentages=dict(get_clocks(problem)),
+                         rates=dict(get_rates(problem))) as exact:
+            exact.constraints(*constraints)
+            exact.minimize(exact.building_total)
+            exact.solve()
+            exact.print()
+
+        approx = exact
+
+    exact.graph()
